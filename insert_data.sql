7526cf5fa45c8a5a2f88d2a19dd2d464d24b609d
Joshua Lee <joshuadmlee@gmail.com>
2025-04-03 15:32:48 -0700

    added insert_data
    allows query 6 to have results

---
+++ insert_data.sql
@@ -0,0 +1,7 @@
+-- Inserts future appointments for Query 6 to return results
+INSERT INTO Appointments (AppointmentID, Date, Time, PatientID, DoctorID)
+VALUES (99901, '2025-07-06', '10:00:00', 596, 589);
+INSERT INTO Appointments (AppointmentID, Date, Time, PatientID, DoctorID)
+VALUES (99902, '2025-08-10', '13:30:00', 160, 894);
+INSERT INTO Appointments (AppointmentID, Date, Time, PatientID, DoctorID)
+VALUES (99903, '2025-10-13', '15:00:00', 212, 778);
